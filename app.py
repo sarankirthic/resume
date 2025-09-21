3774db0ababf1d1d732b7f137ce51da881a8095d
saran <spskartist@gmail.com>
2025-09-21 13:33:45 +0530

    defined function to serve resume

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import send_file
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/helloworld')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
+@app.route('/')
+def serve_resume():
+    resume_path = 'pdf/sarankirthic.pdf'
+    return send_file(resume_path, mimetype='application/pdf')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
